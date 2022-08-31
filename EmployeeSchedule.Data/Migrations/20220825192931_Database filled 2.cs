cc98ab02e3cf0cf30f287cfd5c8dd5c4a927554d
RadivojkaObucina <76410434+RadivojkaObucina@users.noreply.github.com>
2022-09-01 01:15:04 +0200

    Izmenjena migracija za punjenje baze

--- EmployeeSchedule.Data/Migrations/20220825192931_Database filled 2.cs
+++ EmployeeSchedule.Data/Migrations/20220825192931_Database filled 2.cs
@@ -11,9 +11,9 @@ namespace EmployeeSchedule.Data.Migrations
                 columns: new[] { "Name", "Surname", "Adress", "Number", "Email", "Password", "Possition", "CompanyId", "Administrator", "CityName" },
                 values: new object[,] {
                     {"Andrea", "Pesic", "Miluna Ivanovica 2", "0692108099", "deapesic@live.com", "andrea", "Digital team member", 2, true, "Kraljevo" },
-                    {"Maja", "Petrovic", "Stevana Sremca 53", "066545545", "petrovic.maja99@gmail.com", "maja", "Digital team member", 2, false, "Pirot" },
-                    {"Iva", "Petkovic", "Omladinska 9", "0603366920", "ivaa99petkovic@gmail.com", "iva", "BI Department Intern", 3, false, "Zajecar" },
-                    {"Rada", "Obucina", "Vilovi bb", "0603364296", "radaobucina@gmail.com", "rada", "BI Department Intern", 3, false, "Nova Varos" },
+                    {"Maja", "Petrovic", "Stevana Sremca 53", "066545545", "petrovic.maja99@gmail.com", "majapet", "Digital team member", 2, false, "Pirot" },
+                    {"Iva", "Petkovic", "Omladinska 9", "0603366920", "ivaa99petkovic@gmail.com", "ivapet", "BI Department Intern", 3, false, "Zajecar" },
+                    {"Rada", "Obucina", "Vilovi bb", "0603364296", "radaobucina@gmail.com", "radaobu", "BI Department Intern", 3, false, "Nova Varos" },
                 }
                 );
         }
